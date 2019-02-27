3d0857d9f60914588b9fa29a2ebe52a7654a93a8
danielrustrum <danieltrustrum@gmail.com>
2019-02-27 07:56:58 -0700

    Created HTML structure

--- public/index.js
+++ public/index.js
@@ -4,15 +4,7 @@ let OnClick = (selector, event) => {
     return tag;
 }
 
-let changeLocationOnClick = (selector, url) => {
-    OnClick(selector, () => {
-        window.location.href = url;
-    });
-}
 
 document.addEventListener("DOMContentLoaded", () => {
-    changeLocationOnClick("linked-in","https://www.linkedin.com/in/daniel-rustrum-771506117");
-    changeLocationOnClick("medium","https://medium.com/@danieltrustrum");
-    changeLocationOnClick("github","https://github.com/DanielRustrum");
-    changeLocationOnClick("hack-the-box","https://www.hackthebox.eu/home/users/profile/104385");
+    //onclick();
 });
